38001e0a731e996f6025335c223dd5b21e85d2e1
jps2002 <certicado@outlook.com>
2021-06-01 13:25:05 -0300

    Realizar lab5

--- TuiterLite/src/Tuite.java
+++ TuiterLite/src/Tuite.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tuite {
 
@@ -14,7 +18,29 @@ public class Tuite {
     }
 
     public void anexarAlgo(Object anexo) {
-        // ToDo IMPLEMENT ME!!!!
+        this.anexo = anexo;
+    }
+
+    /**
+     * Encontra as hashtags em um texto.
+     * @param texto a ser analisado e de onde serão extraídas as hashtags
+     * @return hashset com as hashtags em strings separadas
+     */
+    public static Set<String> findHashtags(String texto)
+    {
+        // criar novo hashset
+        Set<String> hashtagsEncontradas = new HashSet<String>();
+
+        // adicionar as hashtags encontradas em texto ao hashset criado
+        Pattern padrao = Pattern.compile("(#+[a-zA-Z0-9(_)]+)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = padrao.matcher(texto);
+        while(matcher.find()) {
+            String hashtag = matcher.group(1);
+            hashtagsEncontradas.add(hashtag);
+        }
+
+        // retornar o hashset com as hashtags encontradas
+        return hashtagsEncontradas;
     }
 
     public Object getAnexo() {
@@ -31,5 +57,27 @@ public class Tuite {
 
     public Set<String> getHashtags() {
         return this.hashtags;
+    }
+
+    public static void main(String[] args) {
+
+        // Testes para encontrar o melhor algoritmo que utiliza regex para encontrar hashtags em strings
+
+        // usando:(#+[a-zA-Z0-9(_)]+)
+        Pattern padrao = Pattern.compile("(#+[a-zA-Z0-9(_)]+)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = padrao.matcher("oi #hello joao #pedro#joao");
+        while(matcher.find()) {
+            String hashtag = matcher.group(1);
+            System.out.println("Hashtag encontrada: " + hashtag);
+        }
+
+        //usando: caracteres de escape
+        Pattern padrao2 = Pattern.compile(".*?(#+\\w+).*?");
+        Matcher matcher2 = padrao2.matcher("oi #hello joao #pedro#joao");
+        while(matcher2.find())
+        {
+            String hashtag = matcher2.group(1);
+            System.out.println("Hashtag encontrada: " + hashtag);
+        }
     }
 }
--- TuiterLite/src/TuiterLite.java
+++ TuiterLite/src/TuiterLite.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+// Resolução por João Pedro Silveira Gonçalves - DRE 120019402
 /**
  *  Esta classe implementa um sistema de mensagens curtas estilo Twitter.
  *  É preciso cadastrar um usuário, identificado pelo seu e-mail, para que tuítes possam ser feitos.
@@ -11,6 +14,25 @@ public class TuiterLite {
 
     public static int TAMANHO_MAXIMO_TUITES = 120;
 
+    // atributos
+
+    private HashMap<String, Usuario> usuarios; // o conjunto de usuários identificados por seu email
+    private ArrayList tuites; // o conjunto de tuites feitos pelos os usuários
+    private HashMap<String, Integer> hashtags; // o conjunto de hashtags usadas identificadas por seu nome(ex: #LAB7) e qual sua frequência
+
+
+    // construtor
+
+    public TuiterLite()
+    {
+        this.usuarios = new HashMap<String, Usuario>(); // criar uma hashmap para guardar usuarios
+        this.tuites = new ArrayList(); // criar uma arrayList para guardar tuites
+        this.hashtags = new HashMap<>(); // criar um hashmap para guardar hashtags
+    }
+
+    // métodos
+
+
     /**
      * Cadastra um usuário, retornando o novo objeto Usuario criado.
      * @param nome O nome do usuário.
@@ -18,8 +40,18 @@ public class TuiterLite {
      * @return O Usuario criado.
      */
     public Usuario cadastrarUsuario(String nome, String email) {  // throws UsuarioJaExisteException {
-        // ToDo IMPLEMENT ME!!!
-        return null;
+
+        // se o usuario não estiver cadastrado, criar usuario e adicionar a lista de usuários
+        if(!usuarios.containsKey(email))
+        {
+            Usuario novoUsuario = new Usuario(nome, email); // criar um usuário
+            this.usuarios.put(email, novoUsuario); // adicionar o novo usuário à mapa de usuários dessa instância de tuiter lite
+        }
+
+        // retornar o Usuario do mapa de usuários relacionado ao email dado
+        // ou seja, o usuário novo criado,caso ele não estivesse previamente cadastrado
+        // ou um usuário preexistente, caso ele já tivesse sido cadastrado previamente
+        return usuarios.get(email);
     }
 
     /**
@@ -31,7 +63,27 @@ public class TuiterLite {
      * PS.: Se o texto exceder o limite pré-definido, ou o usuário não estiver cadastrado, return null
      */
     public Tuite tuitarAlgo(Usuario usuario, String texto) {
-        // ToDo IMPLEMENT ME!!!
+
+        // se o texto estiver no tamanho certo pré-definido:
+        if(texto.length() <= TAMANHO_MAXIMO_TUITES)
+        {
+            // se o usuário estiver cadastrado no mapa de usuários dessa instância de tuiter lite
+            if(usuarios.containsKey(usuario.getEmail()))
+            {
+                // encontrar hashtags no texto
+                Set<String> hashtagsEncontradas = Tuite.findHashtags(texto);
+                // com as informações passadas(usuário e texto) e as hashtags encontradas no texto, criar um novo tuite
+                Tuite novoTuite = new Tuite(usuario, texto, hashtagsEncontradas);
+                // adicionar novo tuite ao sistema
+                tuites.add(novoTuite);
+                // adicionar hashtags ao sistema
+                inserirHashtags(hashtagsEncontradas);
+                // retornar o novo tuite
+                return novoTuite;
+
+            }
+        }
+        // se o texto está fora do limite ou o usuário não foi cadastrado previamente, retornar null
         return null;
     }
 
@@ -41,7 +93,40 @@ public class TuiterLite {
      * @return A hashtag mais comum, ou null se nunca uma hashtag houver sido tuitada.
      */
     public String getHashtagMaisComum() {
-        // ToDo IMPLEMENT ME!!!
+
+        // encontrar a maior frequência de uma hashtag cadastrada(dentre as previamente utilizadas)
+        int maiorFrequenciaHashtag = Collections.max(hashtags.values());
+
+        // iterar pelo hashmap das hashtags até encontrar a primeira hashtag que têm essa frequência
+        for (Map.Entry<String, Integer> hashtag : hashtags.entrySet())
+        {
+            if(hashtag.getValue() == maiorFrequenciaHashtag)
+            {
+                return hashtag.getKey();
+            }
+        }
+
+        // se não encontrou, nunca uma hashtag foi tuitada e devemos retornar null
         return null;
+    }
+
+    /**
+     * Atualizar o sistema com as hashtags encontradas em um novo tuite.
+     * @param hashtagsEncontradas no texto do novo tuite.
+     */
+    private void inserirHashtags(Set<String> hashtagsEncontradas)
+    {
+        // para cada hashtag encontrada no texto
+        for (String hashtag: hashtagsEncontradas)
+        {
+            // se ela não estiver cadastrada
+            if(!hashtags.containsKey(hashtag))
+            {
+                // cadastrá-la com frequência 0
+                hashtags.put(hashtag, 0);
+            }
+            // atualizar sua frequência(agora com todas hashtags encontradas já cadastradas)
+            hashtags.put(hashtag, hashtags.get(hashtag) + 1 );
+        }
     }
 }
